ed626d7eab1a17c8ef8bc3e946ec0b40fd8ce7a2
saniatariq388 <saniatariq388@gmail.com>
2024-11-21 01:31:00 +0500

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,29 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  
+     <>
+      {/* <div className="flex gap-[33px]">
+      <div className="shrink-0 w-1/3 h-[200px] bg-pink-400"></div>
+      <div className="shrink-0 w-1/3 h-[200px] bg-purple-400"></div>
+      <div className="shrink-0 w-1/3 h-[200px] bg-red-400"></div>
+      </div> */}
+      {/* <div className="flex">
+      <div className="shrink-0 w-[100px] h-[200px] bg-pink-400"></div>
+      <div className="shrink-0 w-[200px] h-[200px] bg-purple-400"></div>
+      <div className="shrink-0 w-full h-[200px] bg-red-400"></div>
+      // </div> */}
+
+      <div className="overflow-hidden flex flex-wrap gap-x-[33px] gap-y-[22px]"></div>
+      
+      <div className="flex flex-wrap gap-[33px]">
+      <div className="shrink-0 w-[100px] h-[200px] bg-pink-400"></div>
+      <div className="shrink-0 w-[200px] h-[200px] bg-purple-400"></div>
+       <div className="grow h-[200px] bg-red-400"></div> 
+       {/* // grow say wo bachi hui parent ki jaga py phail jay ga bina scroll bar width m lay bagair */}
+        <div className="shrink-0 grow min-w-[1200px] max-w-[1200px] h-[200px] bg-red-400"></div> 
+       {/* na 1200 sy chota hoga na min m bara hoga */}
+
+      </div>
+     </> 
   );
 }
